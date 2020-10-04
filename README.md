b0a92e8229a70c88915b7a47924e43cfc13d58cf
Bryan Ricker <bryancricker@gmail.com>
2020-10-04 12:19:51 -0700

    Add headline and byline extractors

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 A chrome extension which makes reading news better.
 
 It removes all javascript (including paywalls), CSS, images, ads, etc. from popular news sites (eg. CNN, Washington Post). All that's left is the body of the article.
+
+## Roadmap
+
+1. Some attempt at some sort of styling
+3. Control Pane (selectively toggle websites, toggle media)
+4. Automated integration tests
+5. Something that's not regular expressions
+6. An iOS App
+7. An Android App
--- src/index.js
+++ src/index.js
@@ -3,47 +3,73 @@
 const extractors = [
   {
     name: 'CNN',
-    urlMatcher: /www\.cnn\.com/,
-    paywallMatcher: null,
-    querySelector: '.zn-body__paragraph',
+    urlMatchers: [/cnn\.com/],
+    paywallMatchers: [],
+    headlineSelectors: ['.pg-headline'],
+    bylineSelectors: ['.metadata__info'],
+    dateSelectors: [],
+    bodySelectors: ['.zn-body__paragraph'],
   },
 
   {
     name: 'LA Times',
-    urlMatcher: /latimes\.com/,
-    paywallMatcher: 'Subscribe to continue reading',
-    querySelector: '.RichTextArticleBody-body>p',
+    urlMatchers: [/latimes\.com/],
+    paywallMatchers: ['Subscribe to continue reading'],
+    headlineSelectors: ['.headline'],
+    bylineSelectors: ['.authors'],
+    dateSelectors: ['.published-date'],
+    bodySelectors: [
+      '.RichTextArticleBody-body>p',
+      '.page-article-body p',
+    ],
   },
 
   {
     name: 'New York Times',
-    urlMatcher: /nytimes\.com/,
-    paywallMatcher: 'Keep reading The Times by creating a free account or logging in.',
-    querySelector: 'section[name=articleBody]>div>div>p',
+    urlMatchers: [/nytimes\.com/],
+    paywallMatchers: ['Keep reading The Times by creating a free account or logging in.'],
+    headlineSelectors: ['*[data-test-id=headline]'],
+    bylineSelectors: ['*[itemprop=author] span[itemprop=name]'],
+    dateSelectors: ['article header time>span'],
+    bodySelectors: ['section[name=articleBody]>div>div>p'],
   },
 
   {
     name: 'Washington Post',
-    urlMatcher: /washingtonpost\.com/,
-    paywallMatcher: 'Subscribe to read the full article',
-    querySelector: '.article-body p',
+    urlMatchers: [/washingtonpost\.com/],
+    paywallMatchers: ['Subscribe to read the full article'],
+    headlineSelectors: ['header h1'],
+    bylineSelectors: ['article span.author-name'],
+    dateSelectors: ['article .font--subhead>.display-date'],
+    bodySelectors: ['.article-body p'],
   },
 ];
 
 const resolver = {
   getExtractorForUrl(url) {
-    return extractors.find((extractor) => url.match(extractor.urlMatcher));
+    return extractors.find((extractor) => extractor.urlMatchers.some((matcher) => url.match(matcher)));
   },
 };
 
 const writer = {
-  write(paragraphs /* NodeList */) {
-    const newPgs = [];
-    paragraphs.forEach((pg) => newPgs.push(`<p>${pg.innerHTML}</p>`));
+  write({ headlineNodes, bylineNodes, dateNodes, paragraphNodes }) {
+    console.debug('[news-reader] Writing');
 
     document.open();
-    document.write("<html><body><div style='width:600px'>");
-    document.write(newPgs.join('\n\n'));
+    document.write("<html><body><div style='width:600px;margin:auto'>");
+
+    headlineNodes.forEach((node) => { document.write(`<h1>${node.innerHTML}</h1>`); });
+
+    const authors = [];
+    bylineNodes.forEach((node) => { authors.push(node.innerHTML); });
+    document.write(`<p>AUTHORS: ${authors.join(', ')}</p>`);
+
+    dateNodes.forEach((node) => { document.write(`<p>${node.innerHTML}</p>`); });
+
+    document.write('<hr>');
+
+    paragraphNodes.forEach((node) => { document.write(`<p>${node.innerHTML}</p>`); });
+
     document.write('</div></body></html>');
     document.close();
   },
@@ -54,18 +80,53 @@ function run() {
     if (!items.enabled) { return; }
 
     const extractor = resolver.getExtractorForUrl(window.location.hostname);
-    if (extractor) {
-      const pgs = document.querySelectorAll(extractor.querySelector);
+    if (!extractor) {
+      console.warn('[news-reader] No extractor available for this URL.');
+      return;
+    }
 
-      if (pgs.length > 0) {
-        writer.write(pgs, document);
+    console.debug('[news-reader] Found extractor', extractor);
 
-        if (extractor.paywallMatcher && document.body.innerText.match(extractor.paywallMatcher)) {
-          window.location.reload();
-        }
-      }
-    } else {
-      console.warn('No extractor available for this URL.'); // eslint-disable-line no-console
+    let paragraphNodes = [];
+    const foundPgs = extractor.bodySelectors.some((querySelector) => {
+      paragraphNodes = document.querySelectorAll(querySelector);
+      return paragraphNodes.length > 0;
+    });
+
+    if (!foundPgs) {
+      console.warn('[news-reader] None of the query selectors worked.');
+      return;
+    }
+
+    let headlineNodes = [];
+    extractor.headlineSelectors.some((querySelector) => {
+      headlineNodes = document.querySelectorAll(querySelector);
+      return headlineNodes.length > 0;
+    });
+
+    let bylineNodes = [];
+    extractor.bylineSelectors.some((querySelector) => {
+      bylineNodes = document.querySelectorAll(querySelector);
+      return bylineNodes.length > 0;
+    });
+
+    let dateNodes = [];
+    extractor.dateSelectors.some((querySelector) => {
+      dateNodes = document.querySelectorAll(querySelector);
+      return dateNodes.length > 0;
+    });
+
+    writer.write({ headlineNodes, bylineNodes, dateNodes, paragraphNodes });
+
+    // Now check if a paywall is still present. We use this as an indicator that something didn't work.
+    const foundPaywall = extractor.paywallMatchers.some((paywallMatcher) => {
+      return document.body.innerText.match(paywallMatcher);
+    });
+
+    if (foundPaywall) {
+      console.debug('[news-reader] Paywall detected. Reloading page.');
+      window.location.reload();
+      return;
     }
   });
 }
